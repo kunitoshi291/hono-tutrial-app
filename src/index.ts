70fab87fc2c5a3de8a7a2070589ccbf74e6af963
Takekuni <takevadogu@outlook.jp>
2024-02-19 23:25:58 +0900

    ベーシック認証のミドルウェアを実装の写経

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,32 @@
 import { Hono } from 'hono'
 import { prettyJSON } from 'hono/pretty-json';
 import posts from "./blogs/blogs"
+import auth from "./auth/auth";
+import { basicAuth as basicAuthMiddleware } from 'hono/basic-auth'
+
 
 const app = new Hono();
 
 app.use("*", prettyJSON());
+
+app.use(
+  '/auth/*',
+  basicAuthMiddleware({
+    username: 'toshi',
+    password: 'toshi',
+  })
+)
+
 app.route("/posts", posts);
+app.route("/auth", auth);
+
 
 app.get('/', (c) => {
   return c.text('!')
 });
 
 export default app
+function basicAuth(arg0: { username: string; password: string; }): import("hono").MiddlewareHandler<import("hono").Env, "/auth/*", {}> {
+  throw new Error('Function not implemented.');
+}
+
